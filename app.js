6c0f835943a1988a859127f7de8ac429676bcc84
Jean-Baptiste Lavisse <jean-baptiste@jblavisse.fr>
2021-04-29 12:08:39 +0200

    Hi budgie

--- app.js
+++ app.js
@@ -5,15 +5,31 @@ class App extends React.Component {
         age: 12
     }
 
+    hello() {
+        console.log("coucouuuuuuu");
+    }
+
+    hoverboard() {
+        console.log("IL A UN HOVERBOARDDDDDDDD...MCFLY!");
+    }
+
+    hiBudgie = () => {
+        console.log("coucou petite perruche!");
+        console.log(this.state.age);
+    }
+
     render() {
 
         return (
             <section>
-                <h1>Youhou la familleeeeeee! {Math.random()}</h1>
-                <p>Lorem ipsum dolor {"sit amet".toUpperCase()} consectetur adipisicing elit. A, accusantium?</p>
+                <h1 onClick={this.hello} onMouseOver={this.hoverboard}>Youhou la familleeeeeee!</h1>
+                
                 <a href="https://www.google.fr">On souffre</a>
 
-                <h2 className="bidule">J'ai {this.state.age} ans</h2>
+                <h2 className="bidule"><span onClick={this.hello}>J'ai</span> {this.state.age} ans</h2>
+                
+
+                <button onClick={this.hiBudgie}>Mon vieil élan</button>
             </section>
 
         )
